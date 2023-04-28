1dd2f136dcb339dac8a67bfb17e8953c0a195440
Evann BACALA <dev@evannbacala.fr>
2023-04-28 17:10:43 +0200

    🐛 Fixed redirection after the animation ending

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,44 @@
 <template>
-    <h1 class="text-center my-5 animate-h1">Welcome to your backoffice</h1>
+  <h1 class="text-center my-5 animate-h1">Welcome to your backoffice</h1>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
+let timeoutAnimation = ref(0)
+
 onMounted(() => {
-    setTimeout(() => router.push({ name: 'learners' }), 5000)
+  timeoutAnimation.value = setTimeout(() => router.push({ name: 'learners' }), 5000)
 })
+
+onBeforeUnmount(() => clearTimeout(timeoutAnimation.value))
 </script>
 
 <style scoped>
 @keyframes animation-h1 {
-    0% {
-        opacity: 0;
-        transform: translate(-100%, 0);
-    }
-    50% {
-        opacity: 1;
-        transform: translate(0, 0);
-    }
-    100% {
-        opacity: 0;
-        transform: translate(0, 100%);
-    }
+  0% {
+    opacity: 0;
+    transform: translate(-100%, 0);
+  }
+  50% {
+    opacity: 1;
+    transform: translate(0, 0);
+  }
+  100% {
+    opacity: 0;
+    transform: translate(0, 100%);
+  }
 }
 
 h1 {
-    font-size: clamp(30px, 4vw, 52px);
-    font-weight: 800;
+  font-size: clamp(30px, 4vw, 52px);
+  font-weight: 800;
 }
 
 .animate-h1 {
-    animation: animation-h1 4s linear 0.5s 1 normal both;
+  animation: animation-h1 4s linear 0.5s 1 normal both;
 }
 </style>
